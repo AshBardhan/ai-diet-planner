8efde258e6fbeb652df23034f756cabebb93acae
AshBardhan <ashbardhan@gmail.com>
2024-01-18 09:14:22 +0530

    [Frontend] Apply 'prettier' on some component files

--- frontend/src/Components/BlockList.js
+++ frontend/src/Components/BlockList.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React from 'react';
 import './BlockList.css';
 
-export default function BlockList({ themes=[], list}){
-    const setClassname = () => {
-        let className = 'block-list';
-        if(themes) {
-            className += ` ${themes.map(theme => `block-list--${theme}`).join(' ')}`;
-        }
-        return className;
-    }
-    return(
-        <ul className={setClassname()}>
-            {list.map(listItem =>(<li>{listItem}</li>))}
-        </ul>
-    )
+export default function BlockList({themes = [], list}) {
+	const setClassname = () => {
+		let className = 'block-list';
+		if (themes) {
+			className += ` ${themes.map((theme) => `block-list--${theme}`).join(' ')}`;
+		}
+		return className;
+	};
+	return (
+		<ul className={setClassname()}>
+			{list.map((listItem) => (
+				<li>{listItem}</li>
+			))}
+		</ul>
+	);
 }
--- frontend/src/Components/Button.js
+++ frontend/src/Components/Button.js
@@ -1,23 +1,26 @@
-import React from "react";
+import React from 'react';
 import './Button.css';
 
-export default function Button({ type='button', size='', theme='', isInverted = false, label, handleClick }){
-    const setClassname = () => {
-        let className = 'btn';
-        if(size) {
-            className += ` btn--${size}`;
-        }
-        if(theme) {
-            className += ` btn--${theme}`;
-        }
-        if(isInverted) {
-            className += ` btn--inverted`;
-        }
-        return className;
-    }
-    return(
-        <button type={type} onClick={handleClick} className={setClassname()} >
-            {label}
-        </button>
-    )
+export default function Button({type = 'button', size = '', theme = '', isInverted = false, label, handleClick}) {
+	const setClassname = () => {
+		let className = 'btn';
+		if (size) {
+			className += ` btn--${size}`;
+		}
+		if (theme) {
+			className += ` btn--${theme}`;
+		}
+		if (isInverted) {
+			className += ` btn--inverted`;
+		}
+		return className;
+	};
+	return (
+		<button
+			type={type}
+			onClick={handleClick}
+			className={setClassname()}>
+			{label}
+		</button>
+	);
 }
